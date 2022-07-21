bb78fb27a16c41d4869b4c4af88d854db6cd263d
AYUSH AGARWAL <100485141+Ayushagarwall007@users.noreply.github.com>
2022-07-22 02:25:43 +0530

    Create Remove aLL occurences of a substring LEetcode.cpp

---
+++ String or character arrays/Remove aLL occurences of a substring LEetcode.cpp
@@ -0,0 +1,59 @@
+// Given two strings s and part, perform the following operation on s until all occurrences of the substring part are removed:
+
+// Find the leftmost occurrence of the substring part and remove it from s.
+// Return s after removing all occurrences of part.
+
+// A substring is a contiguous sequence of characters in a string.
+
+
+//APPROACH 01
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+        string s1 = "";
+        int n;
+        int k;
+        //great solution in this case
+        for (int i = 0; i < s.size(); i++)
+        {
+            s1 =s1+ s[i];
+            n = s1.size();
+            k = part.size();
+
+            if (n >= k && s1.substr(n - k) == part )
+            {
+                while (k--)
+                {
+                    s1.pop_back();
+                }
+            }
+        }
+        return s1;
+    }
+};
+
+
+//APPROACH 02
+class Solution {
+public:
+string removeOccurrences(string s, string part) {
+int n=s.length();
+int p=part.length();
+int i=0;
+while(i< n)
+{
+
+        if (s.substr(i,p)==part)
+        {
+            s.erase(i,p);
+            i=0;    
+        }
+        else
+        {
+            i++;
+        }
+        n=s.length();
+    }
+    return s;
+}
+};
