a77402ff18c28fee6439758b17848808c9f960f0
AYUSH AGARWAL <100485141+Ayushagarwall007@users.noreply.github.com>
2022-07-22 02:27:37 +0530

    Update Remove aLL occurences of a substring LEetcode.cpp

--- String or character arrays/Remove aLL occurences of a substring LEetcode.cpp
+++ String or character arrays/Remove aLL occurences of a substring LEetcode.cpp
@@ -57,3 +57,19 @@ while(i< n)
     return s;
 }
 };
+
+
+
+// RECURSIVE APROACH
+class Solution {
+public:
+
+string removeOccurrences(string s, string part) {
+   
+    int found = s.find(part);
+    if(found == -1) return s;
+    s.erase(found , part.size());
+    return removeOccurrences(s, part); 
+return s;
+}
+};
