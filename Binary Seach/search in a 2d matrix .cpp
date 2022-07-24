4026ede4d7abb712a12d77da9c3271f7cf88bb20
AYUSH AGARWAL <100485141+Ayushagarwall007@users.noreply.github.com>
2022-07-24 16:37:22 +0530

    Create search in a 2d matrix .cpp

---
+++ Binary Seach/search in a 2d matrix .cpp
@@ -0,0 +1,39 @@
+// // level 1 problem leetcode
+// Write an efficient algorithm that searches for a value target in an m x n integer matrix matrix. This matrix has the following properties:
+
+// Integers in each row are sorted from left to right.
+// The first integer of each row is greater than the last integer of the previous row
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+       int row=matrix.size();
+        int col= matrix[0].size();
+        int start =0;
+        int end= row*col-1;
+        int mid= start+(end-start)/2;
+        while(start<=end)
+        {
+            int element=matrix[mid/col][mid%col];
+            if(element==target)
+            {
+                return 1;
+            }
+            if(element<target)
+            {
+                start=mid+1;
+            }
+            else
+            {
+                end=mid-1;
+            }
+            mid=start+(end-start)/2;
+            
+        }
+        return 0;
+    }
+};
+
+
+
+// level 2  search in a 2D matrix II leetcode
