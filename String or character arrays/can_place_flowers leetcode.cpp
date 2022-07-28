3ad54535b2c114ef2d0e74aca96a6b0a80a24548
AYUSH AGARWAL <100485141+Ayushagarwall007@users.noreply.github.com>
2022-07-28 23:41:58 +0530

    Update can_place_flowers leetcode.cpp

--- String or character arrays/can_place_flowers leetcode.cpp
+++ String or character arrays/can_place_flowers leetcode.cpp
@@ -1,3 +1,19 @@
+// You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
+
+// Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return if n new flowers can be planted in
+// the flowerbed without violating the no-adjacent-flowers rule.
+
+ 
+
+// Example 1:
+
+// Input: flowerbed = [1,0,0,0,1], n = 1
+// Output: true
+// Example 2:
+
+// Input: flowerbed = [1,0,0,0,1], n = 2
+// Output: false
+
 // '''condition 1 : If current position is not planted (i.e 0) and it's immediate next position is also not planted(i.e 0) then we can plant 1 tree their. so decrement n by 1 since we planted 1 tree and move 2 position next.
 
 // condition 2: If currenet position is planted i.e 1 we need to move forward by 2 position since we can not plant tree adjacent.
