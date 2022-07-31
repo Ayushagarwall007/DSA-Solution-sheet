37d1ab51a2b1889e1b9143470822d16d9f5b052d
AYUSH AGARWAL <100485141+Ayushagarwall007@users.noreply.github.com>
2022-07-31 16:56:55 +0530

    Update Capacity To Ship Packages Within D Days.cpp

--- Binary Seach/Capacity To Ship Packages Within D Days.cpp
+++ Binary Seach/Capacity To Ship Packages Within D Days.cpp
@@ -5,41 +5,52 @@
 
 // Return the least weight capacity of the ship that will result in all the packages on the conveyor belt being shipped within days days.
 
- class Solution {
+class Solution {
 public:
-    bool check(int mid,vector<int>& weights,int days){
-        int c=1,sum=0;
-        for(int i=0;i<weights.size();i++){
-            if(weights[i]>mid){
-                return false;
-            }
-            if(sum+weights[i]>mid){
-                c++;
-                sum=weights[i];
-            }
-            else{
-                sum+=weights[i];
-            }
-            
+bool isAllocation_possible(vector<int>& weights, int days,int max_weight)
+{
+int day=1,current_weight=0;
+for(int i=0;i<weights.size();i++)
+{
+if(current_weight + weights[i]>max_weight)
+{
+day++;
+if(day>days)
+return false;
+current_weight=weights[i];
+}
+else{
+current_weight+=weights[i];
+}
+}
+return true;
+}
+
+int shipWithinDays(vector<int>& weights, int days) {
+    int res=INT_MIN;
+    if(days>weights.size())
+        return -1;
+    
+    int l=*max_element(weights.begin(), weights.end());   
+    int h=accumulate(weights.begin(), weights.end(), 0);   // adding each element of array weights
+    
+    while(l<=h)
+    {
+        int mid=l+(h-l)/2;
+        if(isAllocation_possible(weights,days,mid))
+        {
+            res=mid;
+            h=mid-1;
         }
-        return c<=days;
+        else
+        {
+            l=mid+1;
+        }
     }
-    int shipWithinDays(vector<int>& weights, int days) {
-        int sum=0,v;
-        for(auto i:weights){
-            sum+=i;
-        }
-        int i=*max_element(weights.begin(),weights.end()),j=sum;
-        while(i<=j){
-            int mid = i + (j-i)/2;
-            if(check(mid,weights,days)){
-                v=mid;
-                j=mid-1;
-            }
-            else{
-                i=mid+1;
-            }
-        }
-        return v;
-    }
+    return res;
+    
+}
+};
+
+
 };
