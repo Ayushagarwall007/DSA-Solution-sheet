5ddc6227cab7bf572bf3eb0ceed4debb546d080c
AYUSH AGARWAL <100485141+Ayushagarwall007@users.noreply.github.com>
2022-11-03 11:27:06 +0530

    Create Longest Palindrome by concatinating the string  of length 2 Leetcode.cpp

---
+++ String or character arrays/Longest Palindrome by concatinating the string  of length 2 Leetcode.cpp
@@ -0,0 +1,73 @@
+// 2131. Longest Palindrome by Concatenating Two Letter Words
+// Medium
+// 1.1K
+// 25
+// Companies
+// You are given an array of strings words. Each element of words consists of two lowercase English letters.
+
+// Create the longest possible palindrome by selecting some elements from words and concatenating them in any order. Each element can be selected at most once.
+
+// Return the length of the longest palindrome that you can create. If it is impossible to create any palindrome, return 0.
+
+// A palindrome is a string that reads the same forward and backward.
+
+ 
+
+// Example 1:
+
+// Input: words = ["lc","cl","gg"]
+// Output: 6
+// Explanation: One longest palindrome is "lc" + "gg" + "cl" = "lcggcl", of length 6.
+// Note that "clgglc" is another longest palindrome that can be created.
+// Example 2:
+
+// Input: words = ["ab","ty","yt","lc","cl","ab"]
+// Output: 8
+// Explanation: One longest palindrome is "ty" + "lc" + "cl" + "yt" = "tylcclyt", of length 8.
+// Note that "lcyttycl" is another longest palindrome that can be created.
+// Example 3:
+
+// Input: words = ["cc","ll","xx"]
+// Output: 2
+// Explanation: One longest palindrome is "cc", of length 2.
+// Note that "ll" is another longest palindrome that can be created, and so is "xx".
+ 
+
+// Constraints:
+
+// 1 <= words.length <= 105
+// words[i].length == 2
+// words[i] consists of lowercase English letters.
+
+
+
+class Solution {
+public:
+    int longestPalindrome(vector<string>& words) {
+        unordered_map<string,int>mp;
+        int res=0;
+        for(auto i:words)
+        {
+            string check = i;
+            reverse(check.begin(),check.end());
+            if(mp.find(check)!=mp.end())
+            {
+                mp[check]--;
+                res+=4;
+                
+            }
+            else
+            {
+                mp[i]++;
+            }
+
+        }
+        for(auto it:mp)
+        {
+            if(it.second>=1 and it.first[0]==it.first[1])
+            res+=2;
+            break;
+        }
+        return res;
+    }
+};
