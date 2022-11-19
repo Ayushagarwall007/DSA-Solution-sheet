558dcbc0160f3b6a24b2b15a83d008933fefc749
AYUSH AGARWAL <100485141+Ayushagarwall007@users.noreply.github.com>
2022-11-19 14:03:56 +0530

    Create detect Cycle in an undirected graph using BFS and DFS.cpp

---
+++ GRAPHS./detect Cycle in an undirected graph using BFS and DFS.cpp
@@ -0,0 +1,45 @@
+// USING BFS ALGORITYM TO DETECT THE CYCLE IN THE GRAPH
+// TC :- O(N+2E)
+// SP :- O(C)
+
+
+   bool detect(int src, vector<int> adj[],int vis[])
+    {
+        vis[src]=1;
+        queue<pair<int,int>>q;
+        q.push({src,-1});
+        while(!q.empty()){
+            int node=q.front().first;
+            int parent=q.front().second;
+            q.pop();
+            
+            for(auto adjNode:adj[node])
+            {
+                if(!vis[adjNode])
+                {
+                    vis[adjNode]=1;
+                    q.push({adjNode,node});
+                }
+                else if(parent!=adjNode)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+  public:
+    // Function to detect cycle in an undirected graph.
+    bool isCycle(int V, vector<int> adj[]) {
+        // Code here
+        int vis[V]={0};
+        for(int i=0;i<V;i++)
+        {
+            if(!vis[V])
+            {
+                if(detect(i,adj,vis)) return true;
+            }
+        }
+        return false;
+    }
+};
