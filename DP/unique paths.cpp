f9d438c39e2252ce7a7845ea92b81110af72dac7
AYUSH AGARWAL <100485141+Ayushagarwall007@users.noreply.github.com>
2023-02-13 23:05:14 +0530

    Update unique paths.cpp

--- DP/unique paths.cpp
+++ DP/unique paths.cpp
@@ -53,3 +53,24 @@ public:
         
     }
 };
+
+
+
+
+
+
+
+class Solution {
+public:
+int dp[101][101];
+int solve(int i,int j,int m,int n)
+{
+    if(i>=m || j>=n)return 0;
+    if(i==m-1 and j==n-1)return 1;
+    if(dp[i][j])return dp[i][j];
+    return dp[i][j]=solve(i+1,j,m,n)+solve(i,j+1,m,n);
+}
+    int uniquePaths(int m, int n) {
+        return solve(0,0,m,n);
+    }
+};
