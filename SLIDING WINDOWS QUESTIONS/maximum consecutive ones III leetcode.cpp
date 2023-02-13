bc519e3a5fb53db71464a36ef3cec27cfaade6c3
AYUSH AGARWAL <100485141+Ayushagarwall007@users.noreply.github.com>
2023-02-13 16:26:50 +0530

    Create maximum consecutive ones III leetcode.cpp

---
+++ SLIDING WINDOWS QUESTIONS/maximum consecutive ones III leetcode.cpp
@@ -0,0 +1,54 @@
+// Given a binary array nums and an integer k, return the maximum number of consecutive 1's in the array if you can flip at most k 0's.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2
+// Output: 6
+// Explanation: [1,1,1,0,0,1,1,1,1,1,1]
+// Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+// Example 2:
+
+// Input: nums = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], k = 3
+// Output: 10
+// Explanation: [0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1]
+// Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 105
+// nums[i] is either 0 or 1.
+// 0 <= k <= nums.length
+
+
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int i=0;
+        int j=0;
+        int turns=0;
+        int n=nums.size();
+        int ans=INT_MIN;
+        while(j<n)
+        {
+            if(nums[j]==1)j++;
+            else if(nums[j]==0 and turns<k)
+            {
+                j++;
+                turns++;
+            }
+            else if(nums[j]==0 and turns>=k)
+            {
+                while(turns>=k)
+                {
+                    if(nums[i]==0)turns--;
+                    i++;
+                }
+            }
+            ans=max(ans,j-i);
+        }
+        return ans;
+    }
+};
