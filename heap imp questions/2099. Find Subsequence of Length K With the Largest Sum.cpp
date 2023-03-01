79c1ccd11713e78ac7cd535f42a0a30f82c9f4c6
AYUSH AGARWAL <100485141+Ayushagarwall007@users.noreply.github.com>
2023-03-02 00:54:27 +0530

    Create 2099. Find Subsequence of Length K With the Largest Sum.cpp

---
+++ heap imp questions/2099. Find Subsequence of Length K With the Largest Sum.cpp
@@ -0,0 +1,58 @@
+// You are given an integer array nums and an integer k. You want to find a subsequence of nums of length k that has the largest sum.
+
+// Return any such subsequence as an integer array of length k.
+
+// A subsequence is an array that can be derived from another array by deleting some or no elements without changing the order of the remaining elements.
+
+ 
+
+// Example 1:
+
+// Input: nums = [2,1,3,3], k = 2
+// Output: [3,3]
+// Explanation:
+// The subsequence has the largest sum of 3 + 3 = 6.
+// Example 2:
+
+// Input: nums = [-1,-2,3,4], k = 3
+// Output: [-1,3,4]
+// Explanation: 
+// The subsequence has the largest sum of -1 + 3 + 4 = 6.
+// Example 3:
+
+// Input: nums = [3,4,3,3], k = 2
+// Output: [3,4]
+// Explanation:
+// The subsequence has the largest sum of 3 + 4 = 7. 
+// Another possible subsequence is [4, 3].
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 1000
+// -105 <= nums[i] <= 105
+// 1 <= k <= nums.length
+
+
+class Solution {
+public:
+    vector<int> maxSubsequence(vector<int>& nums, int k) {
+        priority_queue<pair<int,int>> pq;
+    for(int i=0;i<nums.size();i++)
+        pq.push({nums[i],i});
+    
+    vector<int> index;
+    while(k--){
+        pair<int,int> t=pq.top();
+        index.push_back(t.second);
+        pq.pop();
+    }
+    sort(index.begin(),index.end());
+    
+    vector<int> ans;
+    for(int i=0;i<index.size();i++)
+        ans.push_back(nums[index[i]]);
+    
+    return ans;
+    }
+};
