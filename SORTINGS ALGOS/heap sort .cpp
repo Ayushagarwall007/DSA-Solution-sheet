59dd46b76a80f4e7d06a0f57c8c34acd071390d6
AYUSH AGARWAL <100485141+Ayushagarwall007@users.noreply.github.com>
2023-05-14 18:33:55 +0530

    Create heap sort .cpp

---
+++ SORTINGS ALGOS/heap sort .cpp
@@ -0,0 +1,107 @@
+// Heap sort is a comparison-based sorting technique based on Binary Heap data structure. It is similar to the selection sort where we first find the minimum element and place the minimum element at the beginning. 
+//   Repeat the same process for the remaining elements.
+
+
+Important points:
+
+Heap sort is an in-place algorithm. 
+Its typical implementation is not stable, but can be made stable 
+Typically 2-3 times slower than well-implemented QuickSort.  The reason for slowness is a lack of locality of reference.
+  
+  Advantages of heapsort:
+// Efficiency –  The time required to perform Heap sort increases logarithmically while other algorithms may grow exponentially slower as the number of items to sort increases. This sorting algorithm is very efficient.
+// Memory Usage – Memory usage is minimal because apart from what is necessary to hold the initial list of items to be sorted, it needs no additional memory space to work
+// Simplicity –  It is simpler to understand than other equally efficient sorting algorithms because it does not use advanced computer science concepts such as recursion.
+
+
+Disadvantages of Heap Sort:
+// Costly: Heap sort is costly.
+// Unstable: Heap sort is unstable. It might rearrange the relative order.
+// Efficient: Heap Sort are not very efficient when working with highly complex data. 
+
+Applications of HeapSort:
+// Heapsort is mainly used in hybrid algorithms like the IntroSort.
+// Sort a nearly sorted (or K sorted) array 
+// k largest(or smallest) elements in an array 
+
+
+// C++ program for implementation of Heap Sort
+
+#include <iostream>
+using namespace std;
+
+// To heapify a subtree rooted with node i
+// which is an index in arr[].
+// n is size of heap
+void heapify(int arr[], int N, int i)
+{
+
+	// Initialize largest as root
+	int largest = i;
+
+	// left = 2*i + 1
+	int l = 2 * i + 1;
+
+	// right = 2*i + 2
+	int r = 2 * i + 2;
+
+	// If left child is larger than root
+	if (l < N && arr[l] > arr[largest])
+		largest = l;
+
+	// If right child is larger than largest
+	// so far
+	if (r < N && arr[r] > arr[largest])
+		largest = r;
+
+	// If largest is not root
+	if (largest != i) {
+		swap(arr[i], arr[largest]);
+
+		// Recursively heapify the affected
+		// sub-tree
+		heapify(arr, N, largest);
+	}
+}
+
+// Main function to do heap sort
+void heapSort(int arr[], int N)
+{
+
+	// Build heap (rearrange array)
+	for (int i = N / 2 - 1; i >= 0; i--)
+		heapify(arr, N, i);
+
+	// One by one extract an element
+	// from heap
+	for (int i = N - 1; i > 0; i--) {
+
+		// Move current root to end
+//     aab hurr baar max heap se maximum element nikalenge or usko last mai rakh denge or phir se baaki ke element ke liye max element nikalnege
+		swap(arr[0], arr[i]);
+
+		// call max heapify on the reduced heap
+		heapify(arr, i, 0);
+	}
+}
+
+// A utility function to print array of size n
+void printArray(int arr[], int N)
+{
+	for (int i = 0; i < N; ++i)
+		cout << arr[i] << " ";
+	cout << "\n";
+}
+
+// Driver's code
+int main()
+{
+	int arr[] = { 12, 11, 13, 5, 6, 7 };
+	int N = sizeof(arr) / sizeof(arr[0]);
+
+	// Function call
+	heapSort(arr, N);
+
+	cout << "Sorted array is \n";
+	printArray(arr, N);
+}
