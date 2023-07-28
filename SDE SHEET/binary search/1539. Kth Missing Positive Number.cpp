062f43347c065d974d5c10935f65695838d752ad
AYUSH AGARWAL <100485141+Ayushagarwall007@users.noreply.github.com>
2023-07-28 22:07:35 +0530

    Create 1539. Kth Missing Positive Number.cpp

---
+++ SDE SHEET/binary search/1539. Kth Missing Positive Number.cpp
@@ -0,0 +1,50 @@
+// Given an array arr of positive integers sorted in a strictly increasing order, and an integer k.
+
+// Return the kth positive integer that is missing from this array.
+
+ 
+
+// Example 1:
+
+// Input: arr = [2,3,4,7,11], k = 5
+// Output: 9
+// Explanation: The missing positive integers are [1,5,6,8,9,10,12,13,...]. The 5th missing positive integer is 9.
+// Example 2:
+
+// Input: arr = [1,2,3,4], k = 2
+// Output: 6
+// Explanation: The missing positive integers are [5,6,7,...]. The 2nd missing positive integer is 6.
+ 
+
+// Constraints:
+
+// 1 <= arr.length <= 1000
+// 1 <= arr[i] <= 1000
+// 1 <= k <= 1000
+// arr[i] < arr[j] for 1 <= i < j <= arr.length
+ 
+
+// Follow up:
+
+// Could you solve this problem in less than O(n) complexity?
+
+
+
+
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        int left = 0, right = arr.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            // arr[mid]-(mid+1) tell the number of missing elements before the mid index..
+            if (arr[mid] - (mid + 1) < k) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        } 
+        return left + k;
+        
+    }
+};
