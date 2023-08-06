33cfe5b2e71f759f2f48409101363c5ab6142a5a
AYUSH AGARWAL <100485141+Ayushagarwall007@users.noreply.github.com>
2023-08-06 22:49:03 +0530

    Create subarray Sum Equals k Leetcode.cpp

---
+++ SDE SHEET/sliding windows/subarray Sum Equals k Leetcode.cpp
@@ -0,0 +1,70 @@
+
+// Companies
+// Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
+
+// A subarray is a contiguous non-empty sequence of elements within an array.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,1,1], k = 2
+// Output: 2
+// Example 2:
+
+// Input: nums = [1,2,3], k = 3
+// Output: 2
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 2 * 104
+// -1000 <= nums[i] <= 1000
+// -107 <= k <= 107
+// Accepted
+// 1M
+// Submissions
+// 2.3M
+
+
+
+
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int,int>mp;
+        int count=0;
+        int sum=0;
+        mp[0]++;
+        for(auto it:nums)
+        {
+            sum+=it;
+            if(mp.find(sum-k)!=mp.end())
+            {
+                count+=mp[sum-k];
+
+            }
+            mp[sum]++;
+        }
+        return count;
+    }
+};
+
+
+// class Solution {
+// public:
+//     int subarraySum(vector<int>& nums, int k) {
+//         int n=nums.size();
+//         int count=0;
+//         for(int i=0;i<n;i++)
+//         {
+//             int sum=0;
+//             for(int j=i;j<n;j++)
+//             {
+//                 sum+=nums[j];
+//                 if(sum==k)count++;
+//             }
+//         }
+//         return count;
+//     }
+// };
