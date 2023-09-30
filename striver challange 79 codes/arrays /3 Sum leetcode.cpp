8fcdda1fde91d1cf7cf3f8750054d623d63bddab
AYUSH AGARWAL <100485141+Ayushagarwall007@users.noreply.github.com>
2023-10-01 00:42:44 +0530

    Create 3 Sum leetcode.cpp

---
+++ striver challange 79 codes/arrays /3 Sum leetcode.cpp
@@ -0,0 +1,71 @@
+
+// Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+// Notice that the solution set must not contain duplicate triplets.
+
+ 
+
+// Example 1:
+
+// Input: nums = [-1,0,1,2,-1,-4]
+// Output: [[-1,-1,2],[-1,0,1]]
+// Explanation: 
+// nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+// nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+// nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+// The distinct triplets are [-1,0,1] and [-1,-1,2].
+// Notice that the order of the output and the order of the triplets does not matter.
+// Example 2:
+
+// Input: nums = [0,1,1]
+// Output: []
+// Explanation: The only possible triplet does not sum up to 0.
+// Example 3:
+
+// Input: nums = [0,0,0]
+// Output: [[0,0,0]]
+// Explanation: The only possible triplet sums up to 0.
+ 
+
+// Constraints:
+
+// 3 <= nums.length <= 3000
+// -105 <= nums[i] <= 105
+
+
+
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> res;
+        if(nums.size() == 0)
+            return res;
+        if(nums.size() < 3)
+            return res;
+	sort(nums.begin(), nums.end());
+	for(int i = 0; i < nums.size()-2; i++)
+	{
+		if(i>0 && nums[i] == nums[i-1])
+			continue;	
+		int l = i + 1, r = nums.size()-1;
+		while(l < r)
+		{
+			int threeSum = nums[i] + nums[l] + nums[r];
+			if(threeSum > 0)
+				r -= 1;
+			else if(threeSum < 0)
+				l += 1;
+			else
+			{
+				vector<int> temp = {nums[i], nums[l], nums[r]};
+				res.push_back(temp);
+				l += 1;
+				while(nums[l] == nums[l-1] and l < r)
+					l += 1;
+			}
+		} 
+	}
+	return res;
+    }
+};
