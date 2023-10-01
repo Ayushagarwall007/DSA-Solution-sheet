d5c1ccba78338ea80b124f27a044046742d7be4e
AYUSH AGARWAL <100485141+Ayushagarwall007@users.noreply.github.com>
2023-10-01 11:11:47 +0530

    Create Koko eating vegetable.cpp

---
+++ striver challange 79 codes/binary  search/Koko eating vegetable.cpp
@@ -0,0 +1,67 @@
+// Koko loves to eat bananas. There are n piles of bananas, the ith pile has piles[i] bananas. The guards have gone and will come back in h hours.
+
+// Koko can decide her bananas-per-hour eating speed of k. Each hour, she chooses some pile of bananas and eats k bananas from that pile. If the pile has less than k bananas, she eats all of them instead and will not eat any more bananas during this hour.
+
+// Koko likes to eat slowly but still wants to finish eating all the bananas before the guards return.
+
+// Return the minimum integer k such that she can eat all the bananas within h hours.
+
+ 
+
+// Example 1:
+
+// Input: piles = [3,6,7,11], h = 8
+// Output: 4
+// Example 2:
+
+// Input: piles = [30,11,23,4,20], h = 5
+// Output: 30
+// Example 3:
+
+// Input: piles = [30,11,23,4,20], h = 6
+// Output: 23
+ 
+
+// Constraints:
+
+// 1 <= piles.length <= 104
+// piles.length <= h <= 109
+// 1 <= piles[i] <= 109
+
+  
+class Solution {
+public:
+bool solve(vector<int>&piles,int h, int mid)
+{
+    long long hrTaken=0;
+    for(auto i:piles)
+    {
+        if(i<=mid)hrTaken++;
+        else
+        {
+            hrTaken+=i/mid;
+            if(i%mid!=0)hrTaken++;
+        }
+    }
+    return hrTaken<=h;
+}
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int start=1;
+        int end=*max_element(piles.begin(),piles.end());
+        int ans=0;
+        while(start<=end)
+        {
+            int mid=start+(end-start)/2;
+            if(solve(piles,h,mid))
+            {
+                ans=mid;
+                end=mid-1;
+            }
+            else
+            {
+                start=mid+1;
+            }
+        }
+        return ans;
+    }
+};
