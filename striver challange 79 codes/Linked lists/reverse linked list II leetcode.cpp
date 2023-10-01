552fb5f14aae9966e9e9655e81551e30356aa6c1
AYUSH AGARWAL <100485141+Ayushagarwall007@users.noreply.github.com>
2023-10-01 23:30:45 +0530

    Create reverse linked list II leetcode.cpp

---
+++ striver challange 79 codes/Linked lists/reverse linked list II leetcode.cpp
@@ -0,0 +1,59 @@
+// Given the head of a singly linked list and two integers left and right where left <= right, reverse the nodes of the list from position left to position right, and return the reversed list.
+
+ 
+
+// Example 1:
+
+
+// Input: head = [1,2,3,4,5], left = 2, right = 4
+// Output: [1,4,3,2,5]
+// Example 2:
+
+// Input: head = [5], left = 1, right = 1
+// Output: [5]
+ 
+
+// Constraints:
+
+// The number of nodes in the list is n.
+// 1 <= n <= 500
+// -500 <= Node.val <= 500
+// 1 <= left <= right <= n
+
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        if (!head || left == right) return head;
+        
+        ListNode dummy(0);
+        dummy.next = head;
+        ListNode* prev = &dummy;
+        
+        for (int i = 0; i < left - 1; ++i) {
+            prev = prev->next;
+        }
+        
+        ListNode* current = prev->next;
+        
+        for (int i = 0; i < right - left; ++i) {
+            ListNode* next_node = current->next;
+            current->next = next_node->next;
+            next_node->next = prev->next;
+            prev->next = next_node;
+        }
+        
+        return dummy.next;
+    }
+};
